a196e166f8e7ff38d49495be30a16000d6b2e496
Jhonatan <jhonatan_rivera270897@hotmail.com>
2023-09-13 18:17:33 +0200

    v0.00

---
+++ resources/views/layouts/theme/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="footer-wrapper">
+    <div class="footer-section f-section-1">
+        <p class="">Oficina General de Admisión - OGAD.</p>
+    </div>
+    <div class="footer-section f-section-2">
+        <p class="">Versión 1.0</p>
+    </div>
+</div>
---
+++ resources/views/layouts/theme/topbar.blade.php
@@ -0,0 +1,14 @@
+<div class="topbar-nav header navbar" role="banner">
+    <nav id="topbar" class="">
+        <ul class="navbar-nav theme-brand flex-row  text-center">
+            <li class="nav-item theme-logo">
+                <a href="index.html">
+                    <img src="assets/img/90x90.jpg" class="navbar-logo" alt="logo">
+                </a>
+            </li>
+            <li class="nav-item theme-text">
+                <a href="index.html" class="nav-link"> SSC </a>
+            </li>
+        </ul>
+    </nav>
+</div>
